67646ce3de33048fcb428e78607027f52ba93f07
Henry Jin <henhenthehenhen@gmail.com>
2023-04-13 23:24:25 -0700

    Added Shading

--- RayTest.cpp
+++ RayTest.cpp
@@ -6,10 +6,25 @@
 
 using namespace std;
 
-color ray_color (const ray& r) {
+double hit_sphere (const point3& center, double radius, const ray& r){
+    vec3 dist = r.origin() - center; //distance from ray to sphere
+    auto a = r.direction().length_squared();
+    auto half_b = dot (dist, r.direction());
+    auto c = dist.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
+    if (discriminant < 0) return -1.0; //doesn't hit 
+    else return (-half_b-sqrt(discriminant))/a; //distance b/w origin of ray and closest hit point
+}
+
+color ray_color (const ray& r){
+    double t = hit_sphere (point3 (0, 0, -1), 0.5, r);
+    if (t > 0.0){ //hit
+        vec3 N = unit_vector (r.at(t) - vec3(0, 0, -1)); //normal vector
+        return 0.5 * color (N.x()+1, N.y()+1, N.z()+1); //color b/w [0, 1] (dimmed by half)
+    } 
     vec3 unit_direction = unit_vector (r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0); //makes it a range from [0, 1]
-    return (1.0 - t) * color (1.0, 1.0, 1.0) + t*color (0.5, 0.7, 1.0); //white (1, 1, 1) and light blue (0.5, 0.7, 1.0) gradient 
+    t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0-t)*color (1.0, 1.0, 1.0) + t*color (0.5, 0.7, 1.0); //background (gradient)
 }
 
 int main() {
@@ -21,12 +36,12 @@ int main() {
     // Camera
     auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
+    auto fdistal_length = 1.0;
 
     auto origin = point3(0, 0, 0);
     auto horizontal = vec3(viewport_width, 0, 0);
     auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, fdistal_length);
 
     // Render
 
