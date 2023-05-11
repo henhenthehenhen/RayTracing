6363c76881b2ce504b9d02223685bd345f2882c3
Henry Jin <henhenthehenhen@gmail.com>
2023-05-11 09:56:18 -0700

    Anti-Aliasing
    Added a new camera (made one earlier but didn't realize the text provided one also) as well as a random generator and a clamp function.

--- Header Files + Main/RayTest.cpp
+++ Header Files + Main/RayTest.cpp
@@ -23,6 +23,7 @@ int main(){
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -30,18 +31,21 @@ int main(){
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
-    camera cam (point3 (0, 0, 0), point3(0, 0, -1), vec3(0, 1, 0), 90, aspect_ratio);
+    camera cam;
 
     // Render
     cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     for (int j = image_height-1; j >= 0; j--){
         cerr << "\rScanlines remaining: " << j << ' ' << flush;
         for (int i = 0; i < image_width; i++){
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
-            ray r = cam.get_ray(u, v);
-            color pixel_color = ray_color (r, world);
-            write_color(cout, pixel_color);
+            color pixel_color (0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; s++){
+                auto u = (i + random_double())/(image_width-1);
+                auto v = (j + random_double())/(image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color (cout, pixel_color, samples_per_pixel);
         }
     }
     cerr << "\nDone.\n";
